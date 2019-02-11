d4d81e19f90ae0858d2498959d0e8148038ff291
Sheena Jessee <calalilym@yahoo.com>
2019-02-11 11:13:11 -0700

    committ the changes

---
+++ scripts/homepage.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	$("#aboutme").click(function () {
+		$('#about').slideToggle();
+	});
+});
+$(document).ready(function () {
+	$("#quote").click(function () {
+		$('#favQuote').slideToggle();
+	});
+});
+$(document).ready(function () {
+	$("#favPets").click(function () {
+		$('#pets').slideToggle();
+	});
+});
